b75497ecc791e4791fad13df7800f2a72fbd44ea
coder-dmin <gzdengmin@126.com>
2016-05-25 19:10:06 +0800

    add first

--- app/src/main/java/com/dengmin/zhuhutest/activity/MainActivity.java
+++ app/src/main/java/com/dengmin/zhuhutest/activity/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends Activity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //换成
         //判断网络连接
         isConnected = NetworkUtils.checkNetworkConnection(this);
 
